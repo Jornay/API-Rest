dae910a5112f3a4db7a8fb602e93c6c8c09b2202
Jornay <lucasdeafg@yahoo.com.br>
2021-02-12 18:57:49 -0300

    atualização de Routers e Controllers

---
+++ src/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router =  express.Router();
+
+//Carregar Rotas
+const indexRoutes = require('./routes/indexRoute');
+const productRoutes = require('./routes/productRoute');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+
+app.use('/' ,indexRoutes);
+app.use('/products' ,productRoutes);
+
+
+
+module.exports = app;
