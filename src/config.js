bb1dab2c717b464f01668be2a92c2f8f42f6008a
Jornay <lucasdeafg@yahoo.com.br>
2021-02-17 23:14:35 -0300

    alteração config.js

--- src/config.js
+++
@@ -1,9 +0,0 @@
-   
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
-
-module.exports = {
-    connectionString: 'mongodb+srv://NodeJs:190581@cluster0.bxkcf.mongodb.net/test',
-    sendgridKey: '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',
-    containerConnectionString: 'SUA CONNECTION STRING'
-}
