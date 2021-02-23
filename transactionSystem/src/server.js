8063bd961951bb9485ff4cf98075682cdf8d07ba
dev-lucas.abreu <lucas.abreu@workdoc.com.br>
2021-02-23 12:54:07 -0300

    correction connection error in server

--- transactionSystem/src/server.js
+++ transactionSystem/src/server.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const userModel = require('./routes/userRoute');
+const userRoute = require('./routes/userRoute');
 const app = express();
 
 
 
 
-mongoose.connect('mongodb+srv://omnistechDB:190581@cluster0.ccswx.mongodb.net/test')
+mongoose.connect('<Your Connection Mongo>')
 
-app.set(3000);
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
 
+app.use('/',userRoute);
 
-const router = express.Router();
-
-
-const route = router.get('/', (request, response) => {
-    return response.send('Testado com sucesso');
+//ABRIR O SERVIDOR WEB (localhost:...)
+app.listen(3000, () =>{
+    console.log("teste")
 })
-
-app.use('/',route);
-app.use('/user', userModel());
 
 
